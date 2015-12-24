4d7df16bf9ee0aefc75d5a25063df0289abd70a3
Fedot <fedotru@gmail.com>
2015-12-24 23:28:05 +0300

    Added new version mdl. Extracted template story component template to file

--- angular/app/story.component.ts
+++ angular/app/story.component.ts
@@ -5,39 +5,7 @@ import {Input} from "angular2/core";
 @Component({
     selector: 'backlog-story',
     inputs: ['story'],
-    template: `
-        <div class="backlog-story mdl-card mdl-shadow--2dp mdl-cell mdl-cell--12-col">
-            <div class="mdl-card__title mdl-card--expand backlog-story-title">
-                <div [class.show]="showBasic == true" class="backlog-story-basic">
-                    <h4>{{story.text}}</h4>
-                </div>
-                <div [class.show]="showRequirements == true" class="backlog-story-requirements">
-                    <h5>Требования</h5>
-                    <ul>
-                        <li *ngFor="#requirement of story.requirements">
-                            <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="checkbox-">
-                                <input type="checkbox" id="checkbox-" class="mdl-checkbox__input">
-                                <span class="mdl-checkbox__label">{{requirement}}</span>
-                            </label>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div class="mdl-card__actions mdl-card--border">
-                <a (click)="toggleShowDetails()" class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
-                    Редактировать
-                </a>
-                <a (click)="toggleShowRequirements()" class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
-                    Требования
-                </a>
-            </div>
-            <div class="mdl-card__menu">
-                <button class="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect">
-                    <i class="material-icons">assignment_late</i>
-                </button>
-            </div>
-        </div>
-    `
+    templateUrl: 'app/templates/story.component.html'
 })
 export class StoryComponent{
     story: Story;
