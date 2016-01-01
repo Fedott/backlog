58a993686e1522f88c7e04aacd728f9ff113688f
Fedot <fedotru@gmail.com>
2016-01-01 20:50:40 +0300

    Added interface for edit story text

--- angular/app/story.component.ts
+++ angular/app/story.component.ts
@@ -2,6 +2,7 @@ import {Component} from 'angular2/core';
 import {Story} from "./story";
 import {Input} from "angular2/core";
 import {Nl2BrPipe} from "./pipes/nl2br.pipe";
+import {ElementRef} from "angular2/core";
 
 @Component({
     selector: 'backlog-story',
@@ -9,12 +10,21 @@ import {Nl2BrPipe} from "./pipes/nl2br.pipe";
     templateUrl: 'app/templates/story.component.html',
     pipes: [Nl2BrPipe]
 })
-export class StoryComponent{
+export class StoryComponent {
     story: Story;
 
     showBasic = true;
     showRequirements = false;
     showDetails = false;
+
+    isEditMode = false;
+    textHeight: String;
+
+    element: ElementRef;
+
+    constructor (myElement: ElementRef) {
+        this.element = myElement;
+    }
 
     toggleShowDetails () {
         this.showDetails = !this.showDetails;
@@ -23,5 +33,36 @@ export class StoryComponent{
     toggleShowRequirements () {
         this.showRequirements = !this.showRequirements;
         this.showBasic = !this.showBasic;
+    }
+
+    toggleEditMode() {
+        if (!this.isEditMode) {
+            this.textHeight = this.element.nativeElement.querySelector('.backlog-story-title-block h4').scrollHeight;
+        }
+
+        this.isEditMode = !this.isEditMode;
+    }
+
+    autoExpand(target) {
+        if (target instanceof HTMLElement) {
+            var calcHeight = target.scrollHeight - 8;
+            target.style.height = calcHeight.toString() + "px";
+        }
+    }
+
+    getTextHeight() {
+        if (null !== this.textHeight) {
+            return this.textHeight + 'px';
+        }
+
+        return '';
+    }
+
+    save() {
+        this.toggleEditMode();
+    }
+
+    cancel() {
+        this.toggleEditMode();
     }
 }
