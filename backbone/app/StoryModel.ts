943b397c450036d92cdaf5a3f247afe7cc89a1c1
Fedot <fedotru@gmail.com>
2016-01-08 03:47:07 +0300

    [front] Added blacklist for storyModel

--- backbone/app/StoryModel.ts
+++ backbone/app/StoryModel.ts
@@ -4,6 +4,13 @@ import {RequirementsCollection} from "./RequirementsCollection";
 export class StoryModel extends Backbone.Model {
     public requirements: RequirementsCollection;
 
+    blacklistProperties() {
+        return [
+            'id',
+            'requirements',
+        ];
+    }
+
     defaults() {
         return {
             text: null,
@@ -33,4 +40,23 @@ export class StoryModel extends Backbone.Model {
 
         return super.parse(response, options);
     }
+
+    save(attributes?:any, options?:ModelSaveOptions):any {
+        options = options || {};
+        options.isSave = true;
+
+        return super.save(attributes, options);
+    }
+
+    toJSON(options?:any):any {
+        if (options.isSave) {
+            return _.omit(super.toJSON(options), this.blacklistProperties());
+        }
+
+        return super.toJSON(options);
+    }
+}
+
+interface ModelSaveOptions extends Backbone.ModelSaveOptions {
+    isSave?: boolean;
 }
