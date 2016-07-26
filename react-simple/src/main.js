cc89453970ea0b5e360c5b150ee47bbf92a468be
Fedot <fedotru@gmail.com>
2016-07-26 22:39:23 +0300

    Added material js and css to bundle self hosted

--- react-simple/src/main.js
+++ react-simple/src/main.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import Application from './components/Application/Application.jsx';
 import webSocketClient from './libraries/WebSocket/WebSocketClient.js';
 
+require("../node_modules/material-design-lite/material.min.css");
+require("../node_modules/material-design-lite/dist/material.indigo-pink.min.css");
+require("../node_modules/material-design-lite/material.min.js");
 require("./style.css");
 
 ReactDOM.render(
