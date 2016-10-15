3e2bb4c7f42ebd728c6a15e98dd3409e08bda5ff
Fedot <fedotru@gmail.com>
2016-10-15 13:35:49 +0300

    Added move for index method

--- amphp/src/Redis/IndexManager.php
+++ amphp/src/Redis/IndexManager.php
@@ -25,32 +25,16 @@ class IndexManager
 
     public function addToIndex($indexName, $id): Promise
     {
-        $promisor = new Deferred();
-
-        \Amp\immediately(function () use ($indexName, $id, $promisor) {
-            yield $this->redisClient->lPush($indexName, $id);
-
-            $promisor->succeed();
-        });
-
-        return $promisor->promise();
+        return $this->redisClient->lPush($indexName, $id);
     }
 
     public function getIdsFromIndex($indexName): Promise
     {
-        $promisor = new Deferred();
-
-        \Amp\immediately(function () use ($promisor, $indexName) {
-            $ids = yield $this->redisClient->lRange(
-                $indexName,
-                0,
-                -1
-            );
-
-            $promisor->succeed($ids);
-        });
-
-        return $promisor->promise();
+        return $this->redisClient->lRange(
+            $indexName,
+            0,
+            -1
+        );
     }
 
     public function addOneToMany(Identifiable $forModel, Identifiable $model): Promise
@@ -65,5 +49,35 @@ class IndexManager
         $indexName = $this->keyGenerator->getOneToManeIndexName($forModel, $modelClassName);
 
         return $this->getIdsFromIndex($indexName);
+    }
+
+    public function removeFromIndex(string $indexName, string $id): Promise
+    {
+        return $this->redisClient->lRem($indexName, $id);
+    }
+
+    public function moveValueOnIndex(string $indexName, string $targetId, string $positionId): Promise
+    {
+        $promisor = new Deferred();
+
+        \Amp\immediately(function () use ($promisor, $targetId, $positionId, $indexName) {
+            yield $this->redisClient->lRem($indexName, $targetId, 1);
+            $insertResult = yield $this->redisClient->lInsert(
+                $indexName,
+                "before",
+                $positionId,
+                $targetId
+            );
+
+            if ($insertResult !== -1) {
+                $promisor->succeed(true);
+            } else {
+                yield $this->redisClient->lPush($indexName, $targetId);
+
+                $promisor->succeed(false);
+            }
+        });
+
+        return $promisor->promise();
     }
 }
