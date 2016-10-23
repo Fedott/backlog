6bf06c64efbf3844abd22a8d8c89a96c088df032
Fedot <fedotru@gmail.com>
2016-10-23 19:21:14 +0300

    Added rewrite rules to index.html

--- amphp/app/aerys-config.php
+++ amphp/app/aerys-config.php
@@ -12,15 +12,17 @@ $websocket       = \Aerys\websocket($webSocketServer);
 
 $router = \Aerys\router()
     ->route('GET', '/websocket', $websocket)
-    ->route('GET', '/assets/bundle.js', function(Request $request, Response $response) {
-        $response->addHeader('content-size', '1042755');
-        $file = file_get_contents(__DIR__."/../web/assets/bundle.js");
-        $response->end($file);
-    })
-    ->route('GET', '/assets/bundle.js.map', function(Request $request, Response $response) {
-        $response->end(file_get_contents(__DIR__."/../web/assets/bundle.js.map"));
-    })
 ;
+
+$reWriter = new class implements \Aerys\Bootable
+{
+    public function boot(\Aerys\Server $server, \Aerys\Logger $logger)
+    {
+        return function (Request $request, Response $response) {
+            $response->end(file_get_contents(__DIR__ . "/../web/index.html"));
+        };
+    }
+};
 
 (new Aerys\Host)
     ->name('backlog.local')
@@ -28,4 +30,5 @@ $router = \Aerys\router()
 //    ->encrypt(__DIR__ . '/keys/crt', __DIR__ . '/keys/key')
     ->use($router)
     ->use($root)
+    ->use($reWriter)
 ;
