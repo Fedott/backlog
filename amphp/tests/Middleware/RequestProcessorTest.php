0267785f090d798da9ce831f09949527b66839a6
Fedot <fedotru@gmail.com>
2016-11-06 03:02:50 +0300

    Added test for RequestProcessor middleware

---
+++ amphp/tests/Middleware/RequestProcessorTest.php
@@ -0,0 +1,66 @@
+<?php declare(strict_types = 1);
+namespace Tests\Fedot\Backlog\Middleware;
+
+use Amp\Success;
+use Fedot\Backlog\Middleware\RequestProcessor;
+use Fedot\Backlog\Request\RequestProcessorManager;
+use Fedot\Backlog\WebSocket\Request;
+use Fedot\Backlog\WebSocket\RequestInterface;
+use Fedot\Backlog\WebSocket\Response;
+use Fedot\Backlog\WebSocket\ResponseInterface;
+use Tests\Fedot\Backlog\BaseTestCase;
+
+class RequestProcessorTest extends BaseTestCase
+{
+    public function testWithoutNext()
+    {
+        $requestProcessorManagerMock = $this->createMock(RequestProcessorManager::class);
+        $requestProcessor = new RequestProcessor($requestProcessorManagerMock);
+
+        $request = new Request(1, 'test', 3);
+        $incomingResponse = new Response($request->getId(), $request->getClientId());
+        $processedResponse = $incomingResponse->withPayload(['test' => 'test']);
+
+        $requestProcessorManagerMock->expects($this->once())
+            ->method('process')
+            ->with($request, $incomingResponse)
+            ->willReturn(new Success($processedResponse))
+        ;
+
+        $actualResponse = \Amp\wait($requestProcessor($request, $incomingResponse));
+
+        $this->assertNotEquals($incomingResponse, $actualResponse);
+        $this->assertEquals($processedResponse, $actualResponse);
+    }
+
+    public function testWithNext()
+    {
+        $requestProcessorManagerMock = $this->createMock(RequestProcessorManager::class);
+        $requestProcessor = new RequestProcessor($requestProcessorManagerMock);
+
+        $request = new Request(1, 'test', 3);
+        $incomingResponse = new Response($request->getId(), $request->getClientId());
+        $processedResponse = $incomingResponse->withPayload(['test' => 'test']);
+        $nextResponse = $processedResponse->withType('next-type');
+
+        $requestProcessorManagerMock->expects($this->once())
+            ->method('process')
+            ->with($request, $incomingResponse)
+            ->willReturn(new Success($processedResponse))
+        ;
+
+        $next = function (RequestInterface $prevRequest, ResponseInterface $prevResponse) use ($request, $incomingResponse, $processedResponse, $nextResponse) {
+            $this->assertNotEquals($prevResponse, $incomingResponse);
+            $this->assertEquals($prevResponse, $processedResponse);
+            $this->assertEquals($request, $prevRequest);
+
+            return new Success($nextResponse);
+        };
+
+        $actualResponse = \Amp\wait($requestProcessor($request, $incomingResponse, $next));
+
+        $this->assertNotEquals($incomingResponse, $actualResponse);
+        $this->assertNotEquals($processedResponse, $actualResponse);
+        $this->assertEquals($nextResponse, $actualResponse);
+    }
+}
