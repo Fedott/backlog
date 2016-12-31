b23a52407071ae7c2ba812d1cff54b34ab8f099c
Fedot <fedotru@gmail.com>
2016-12-31 15:19:23 +0300

    Removed old container. Added parameters for redis from ENV REDIS_URI

--- amphp/app/container.php
+++ amphp/app/container.php
@@ -5,7 +5,9 @@ use Fedot\Backlog\DependencyInjection\Compiler\PropertyInfoPass;
 use Fedot\Backlog\DependencyInjection\Compiler\RequestProcessorPass;
 use Fedot\Backlog\DependencyInjection\Compiler\SerializerPass;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Loader\LoaderResolver;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 $container = new ContainerBuilder();
@@ -15,7 +17,14 @@ $container->addCompilerPass(new SerializerPass());
 $container->addCompilerPass(new PropertyInfoPass());
 $container->addCompilerPass(new RequestProcessorPass());
 
-$loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../app'));
-$loader->load('services.yml');
+$phpLoader = new PhpFileLoader($container, new FileLocator(__DIR__ . '/../app'));
+$yamlLoader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../app'));
+
+$resolver = new LoaderResolver([
+    $phpLoader,
+    $yamlLoader,
+]);
+
+$yamlLoader->load('services.yml');
 
 return $container;
