08b5d7609c1c9a2f200447a0c9849130e7eef963
Fedot <fedotru@gmail.com>
2017-01-27 04:10:48 +0300

    Added Requirement repository with create method

---
+++ amphp/src/Backlog/Repository/RequirementRepository.php
@@ -0,0 +1,56 @@
+<?php declare(strict_types=1);
+
+namespace Fedot\Backlog\Repository;
+
+use function Amp\all;
+use Amp\Deferred;
+use function Amp\wrap;
+use AsyncInterop\Loop;
+use AsyncInterop\Promise;
+use Fedot\Backlog\Model\Requirement;
+use Fedot\Backlog\Model\Story;
+use Fedot\DataStorage\FetchManagerInterface;
+use Fedot\DataStorage\PersistManagerInterface;
+use Fedot\DataStorage\RelationshipManagerInterface;
+
+class RequirementRepository
+{
+    /**
+     * @var PersistManagerInterface
+     */
+    protected $persistManager;
+
+    /**
+     * @var FetchManagerInterface
+     */
+    protected $fetchManager;
+
+    /**
+     * @var RelationshipManagerInterface
+     */
+    protected $relationshipManager;
+
+    public function __construct(
+        PersistManagerInterface $persistManager,
+        FetchManagerInterface $fetchManager,
+        RelationshipManagerInterface $relationshipManager
+    ) {
+        $this->persistManager = $persistManager;
+        $this->fetchManager = $fetchManager;
+        $this->relationshipManager = $relationshipManager;
+    }
+
+    public function create(Story $story, Requirement $requirement): Promise /** @yield bool */
+    {
+        $promisor = new Deferred();
+
+        Loop::defer(wrap(function () use ($promisor, $story, $requirement) {
+            yield $this->persistManager->persist($requirement);
+            yield $this->relationshipManager->addOneToMany($story, $requirement);
+
+            $promisor->resolve(true);
+        }));
+
+        return $promisor->promise();
+    }
+}
---
+++ amphp/tests/Backlog/Repository/RequirementRepositoryTest.php
@@ -0,0 +1,73 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Fedot\Backlog\Repository;
+
+use Amp\Success;
+use function Amp\wait;
+use Fedot\Backlog\Model\Requirement;
+use Fedot\Backlog\Model\Story;
+use Fedot\Backlog\Repository\RequirementRepository;
+use Fedot\DataStorage\FetchManagerInterface;
+use Fedot\DataStorage\PersistManagerInterface;
+use Fedot\DataStorage\RelationshipManagerInterface;
+use PHPUnit_Framework_MockObject_MockObject;
+use Tests\Fedot\Backlog\BaseTestCase;
+
+class RequirementRepositoryTest extends BaseTestCase
+{
+    /**
+     * @var RelationshipManagerInterface|PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $relationshipManagerInterfaceMock;
+
+    /**
+     * @var PersistManagerInterface|PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $persistManagerInterfaceMock;
+
+    /**
+     * @var FetchManagerInterface|PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $fetchManagerInterfaceMock;
+
+    /**
+     * @var RequirementRepository
+     */
+    protected $repository;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->relationshipManagerInterfaceMock = $this->createMock(RelationshipManagerInterface::class);
+        $this->persistManagerInterfaceMock = $this->createMock(PersistManagerInterface::class);
+        $this->fetchManagerInterfaceMock = $this->createMock(FetchManagerInterface::class);
+
+        $this->repository = new RequirementRepository(
+            $this->persistManagerInterfaceMock,
+            $this->fetchManagerInterfaceMock,
+            $this->relationshipManagerInterfaceMock
+        );
+    }
+
+    public function testCreate()
+    {
+        $story = new Story();
+        $requirement = new Requirement('id', 'text');
+
+        $this->persistManagerInterfaceMock->expects($this->once())
+            ->method('persist')
+            ->with($requirement)
+            ->willReturn(new Success(true))
+        ;
+        $this->relationshipManagerInterfaceMock->expects($this->once())
+            ->method('addOneToMany')
+            ->with($story, $requirement)
+            ->willReturn(new Success(true))
+        ;
+
+        $result = wait($this->repository->create($story, $requirement));
+
+        $this->assertTrue($result);
+    }
+}
